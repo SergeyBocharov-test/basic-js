525f42b41268845781399c4bf84b12764826d91a
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-03-29 07:02:58 +0300

    feat: implement count-cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  const matrixFlat = matrix.flat(Infinity)
+  //console.log ('Backyard overview :\n' + matrixFlat.toString().replace(/[,]/g,' '))
+  //console.log ('= = = = = = = = = = = = = = = =')
+  let acc = 0 // accomulator
+  matrixFlat.forEach(element => {
+    //console.log('Element : ' + element)
+    if (element === '^^') {
+      acc++
+      //console.log('Caught a cat!\nTotal cats caught : ' + acc)
+    }
+    //console.log ('- - - -')
+  })
+  //console.log(`Caught ${acc} cats in total.\n`)
+  return acc
 };
