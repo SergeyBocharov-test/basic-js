25acf3e5a6d9ec26da46e877c0cc5a46f3b00c7c
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-03-29 09:01:54 +0300

    feat: implement dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members === [] || 0) return false
+  //console.log(members)
+  let dreamTeam = members
+    .map(item => { 
+      if (typeof(item) === 'string'){
+        item = item.match(/[A-Za-z]/)
+        if (item !== null) return item.toString()
+      } 
+      })
+    .toString()
+    .toUpperCase()
+    .split(',')
+    .sort()
+    .toString()
+    .replace(/[,]/g,'')
+  //console.log('Dream Team name : ' + (dreamTeam ))
+  //console.log('= = = = = = = =\n')
+  return dreamTeam ? dreamTeam : false
+}
+
+//console.log(createDreamTeam(['matt', 'ann', 'dmitry', 'max']), '// Should be ADMM')
+// console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]), '// Should be LOO')
+// console.log(createDreamTeam(123), '// Should be '+ false)
